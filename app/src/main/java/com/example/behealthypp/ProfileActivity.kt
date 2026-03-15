fb95da7d39fc4684bf3124bf8e4ca02c67942013
unknown <amenallah44944@gmail.com>
2026-03-15 13:47:36 +0100

    TP4 :Catalogue statique avec interface

--- app/src/main/java/com/example/behealthypp/ProfileActivity.kt
+++ app/src/main/java/com/example/behealthypp/ProfileActivity.kt
@@ -1,21 +1,19 @@
-package com.example.behealthypp
+package com.example.behealthypp  // Adaptez à votre package
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.behealthypp.R
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        val btnBack = findViewById<Button>(R.id.btnBack)
-        btnBack.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+        val btnCatalogue = findViewById<Button>(R.id.btnCatalogue)
+        btnCatalogue.setOnClickListener {
+            val intent = Intent(this, CatalogueActivity::class.java)
             startActivity(intent)
-            finish() // Optional: closes ProfileActivity
         }
     }
 }
